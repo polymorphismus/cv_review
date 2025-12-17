57805eba79036fc2088d1168ca0526897e0c0b0d
lisa <lisavetpolo@gmail.com>
2025-12-17 17:58:55 +0200

    naming fix

--- main.py
+++ main.py
@@ -10,7 +10,4 @@ load_dotenv()
 if __name__ == "__main__":
     llm = ChatOpenAI(model=MODEL_NAME, temperature=0, api_key=os.environ["NEBIUS_API_KEY"], base_url=os.environ["NEBIUS_BASE_URL"]) 
     graph = build_graph(llm)
-    cv_path = '/Users/lisapolotskaia/Downloads/lisa_polotckaia_cv.pdf'
-    job_description_path = '/Users/lisapolotskaia/Downloads/job_description_ds.txt'
-    result = graph.invoke({'path_to_cv': cv_path, 
-                            'path_to_job': job_description_path}, config=RunnableConfig(checkpointer=MemorySaver()))
+    result = graph.invoke({}, config=RunnableConfig(checkpointer=MemorySaver()))
